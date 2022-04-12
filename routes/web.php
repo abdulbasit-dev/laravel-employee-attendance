df3efbd66c93f7e20bd25a31c4f90f1af4dc0634
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-04-12 11:27:30 +0300

    clean up files and routes

--- routes/web.php
+++ routes/web.php
@@ -43,43 +43,21 @@ use App\Http\Livewire\Users;
 */
 
 Route::redirect('/', '/login');
-
 Route::get('/register', Register::class)->name('register');
-
 Route::get('/login', Login::class)->name('login');
-
 Route::get('/forgot-password', ForgotPassword::class)->name('forgot-password');
-
 Route::get('/reset-password/{id}', ResetPassword::class)->name('reset-password')->middleware('signed');
 
-Route::get('/404', Err404::class)->name('404');
-Route::get('/500', Err500::class)->name('500');
-Route::get('/upgrade-to-pro', UpgradeToPro::class)->name('upgrade-to-pro');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', Profile::class)->name('profile');
-    Route::get('/profile-example', ProfileExample::class)->name('profile-example');
-    Route::get('/users', Users::class)->name('users');
-    Route::get('/login-example', LoginExample::class)->name('login-example');
-    Route::get('/register-example', RegisterExample::class)->name('register-example');
-    Route::get('/forgot-password-example', ForgotPasswordExample::class)->name('forgot-password-example');
-    Route::get('/reset-password-example', ResetPasswordExample::class)->name('reset-password-example');
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
-    Route::get('/transactions', Transactions::class)->name('transactions');
-    Route::get('/bootstrap-tables', BootstrapTables::class)->name('bootstrap-tables');
-    Route::get('/lock', Lock::class)->name('lock');
-    Route::get('/buttons', Buttons::class)->name('buttons');
-    Route::get('/notifications', Notifications::class)->name('notifications');
-    Route::get('/forms', Forms::class)->name('forms');
-    Route::get('/modals', Modals::class)->name('modals');
-    Route::get('/typography', Typography::class)->name('typography');
+    Route::get('/profile', Profile::class)->name('profile');
+
+    // Admin Routes 
+    Route::middleware('is_adimin')->group(function () {
+        Route::resource("departments", DepartmentController::class)->except("show");
+        Route::resource("jobs", JobController::class)->except("show");
+        Route::resource("employees", JobController::class);
+    });
 });
 
 
-// Admin Routes 
-Route::group(["middleware" => ['auth', "is_admin"],], function () {
-
-    Route::resource("departments", DepartmentController::class)->except("show");
-    Route::resource("jobs", JobController::class)->except("show");
-    Route::resource("employees", JobController::class);
-});
