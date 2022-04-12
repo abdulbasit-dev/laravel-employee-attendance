0f8b213a2a35d4bb123d3720b543b446f33d4f04
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-04-12 10:43:02 +0300

    add salary field

--- database/migrations/2022_04_11_075741_create_users_table.php
+++ database/migrations/2022_04_11_075741_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('gender')->nullable();
+            $table->string('sallary')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->string('address')->nullable();
