9e5a2fbabe031a8d5dfdfcabd07350a49d33f40b
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-04-13 15:54:39 +0300

    dashboard values

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Department;
+use App\Models\Job;
+use App\Models\User;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $usersCount;
+    public $deptsCount;
+    public $jobsCount;
     public function render()
     {
+        $this->usersCount  = User::where('is_admin','!=',1)->count();
+        $this->deptsCount  = Department::count();
+        $this->jobsCount  = Job::count();
         return view('dashboard');
     }
 }
